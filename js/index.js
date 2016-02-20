b61db2ac2f21ead6d8f296202b3a6a9bc1b7aba7
JanieHuang <janiexhuang6@gmail.com>
2016-02-20 02:18:30 -0700

    update js

--- js/index.js
+++ js/index.js
@@ -2,71 +2,71 @@
 
 function callCreateDropDown()
 {
-  var type = document.getElementById('timeType');
-  var choice = type.options[type.selectedIndex].value;
+	var type = document.getElementById('timeType');
+	var choice = type.options[type.selectedIndex].value;
 
-  if(choice == "days")
-  {
-    createDropDownDays();
-  }
-  else if(choice == "months")
-  {
-    createDropDownMonths();
-  }
-  else if(choice =="years")
-  {
-    createDropDownYears();
-  }
+	if(choice == "days")
+	{
+		createDropDownDays();
+	}
+	else if(choice == "months")
+	{
+		createDropDownMonths();
+	}
+	else if(choice =="years")
+	{
+		createDropDownYears();
+	}
 }
 
 
 function createDropDownDays()
 {
-  var minDays = 182,
+	var minDays = 182,
     maxDays = 364,
     select = document.getElementById('selectType');
 
-  for (var i = minDays; i<=maxDays; i++)
-  {
-      var op = document.createElement('option');
-      op.value = i;
-      op.innerHTML = i;
-      select.appendChild(op);
-  }
+	for (var i = minDays; i<=maxDays; i++)
+	{
+    	var op = document.createElement('option');
+    	op.value = i;
+    	op.innerHTML = i;
+    	select.appendChild(op);
+	}
 }
 
 function createDropDownMonths()
 {
-  var minMonths = 6,
+	var minMonths = 6,
     maxMonths = 11,
     select = document.getElementById('selectType');
 
-  for (var i = minMonths; i <= maxMonths; i++)
-  {
-      var op = document.createElement('option');
-        op.value = i;
-        op.innerHTML = i;
-      select.appendChild(op);
-  }
+	for (var i = minMonths; i <= maxMonths; i++)
+	{
+   		var op = document.createElement('option');
+  	  	op.value = i;
+  	  	op.innerHTML = i;
+   	 	select.appendChild(op);
+	}
 }
 
 
 function createDropDownYears()
 {
-  var minYears = 1,
+	var minYears = 1,
     maxYears = 99,
     select = document.getElementById('selectType');
 
-  for (var i = minYears; i <= maxYears; i++)
-  {
-      var op = document.createElement('option');
-        op.value = i;
-        op.innerHTML = i;
-      select.appendChild(op);
-  }
+	for (var i = minYears; i <= maxYears; i++)
+	{
+   		var op = document.createElement('option');
+  	  	op.value = i;
+  	  	op.innerHTML = i;
+   	 	select.appendChild(op);
+	}
 }
 
 function clearAllOptions()
 {
-  document.getElementById('selectType').options.length = 0;
+	document.getElementById('selectType').options.length = 0;
 }
