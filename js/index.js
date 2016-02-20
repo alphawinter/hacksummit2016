dc05daca52093356fcce12ed9f7347de4a76499a
JanieHuang <janiexhuang6@gmail.com>
2016-02-20 01:53:52 -0700

    adding js code
    by Chloe

--- js/index.js
+++ js/index.js
@@ -1,8 +1,47 @@
 /*JAVASCRIPT*/
 
-function calculateAllLosses()
-function calculateAllWins()
-function calculateAverage()
-function convertWeeks()
-function convertMonths()
-functioon convertYears()
+function createDropDownDays()
+{
+	var minDays = 182,
+    maxDays = 364,
+    select = document.getElementById('selectDays');
+
+	for (var i = minDays; i<=maxDays; i++)
+	{
+    	var op = document.createElement('option');
+    	op.value = i;
+    	op.innerHTML = i;
+    	select.appendChild(op);
+	}
+}
+
+function createDropDownMonths()
+{
+	var minMonths = 6,
+    maxMonths = 11,
+    select = document.getElementById('selectMonths');
+
+	for (var i = minMonths; i <= maxMonths; i++)
+	{
+   		var op = document.createElement('option');
+  	  	op.value = i;
+  	  	op.innerHTML = i;
+   	 	select.appendChild(op);
+	}
+}
+
+
+function createDropDownYears()
+{
+	var minYears = 1,
+    maxYears = 99,
+    select = document.getElementById('selectYears');
+
+	for (var i = minYears; i <= maxYears; i++)
+	{
+   		var op = document.createElement('option');
+  	  	op.value = i;
+  	  	op.innerHTML = i;
+   	 	select.appendChild(op);
+	}
+}
