e6d485facfca1df40389d3473473c88379d95730
JanieHuang <janiexhuang6@gmail.com>
2016-02-20 04:38:36 -0700

    Update jqueyr

---
+++ js/index-jq.js
@@ -0,0 +1,9 @@
+$( "#welcome" ).click(function() {
+  $( "#welcome" ).animate({
+    opacity: 0.25,
+    up: "+=100",
+    height: "toggle"
+  }, 1000, function() {
+    // Animation complete.
+  });
+});
